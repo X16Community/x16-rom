d8ed7ddc7238926f56aeb4f34df21087c2d6ce4f
mooinglemur <mooinglemur@users.noreply.github.com>
2023-07-12 05:27:53 +0000

    [DOS] Add $=L directory list option (long list) (#146)
    * add ability to get size listing from directory listing
    
    * [DOS] Add $=L directory list option (long list)

--- dos/jumptab.s
+++ dos/jumptab.s
@@ -5,8 +5,6 @@
 
 .import dos_secnd, dos_tksa, dos_acptr, dos_ciout, dos_untlk, dos_unlsn, dos_listn, dos_talk, dos_macptr
 
-.import dos_OpenDisk, dos_ReadBuff, dos_ReadBlock, dos_GetDirHead, dos_CalcBlksFree, dos_Get1stDirEntry, dos_GetNxtDirEntry
-
 .import dos_init, dos_set_time
 
 .segment "dos_jmptab"
@@ -23,13 +21,11 @@
 	jmp dos_talk    ; 7
 
 ; GEOS
-	jmp dos_OpenDisk         ; 8
-	jmp dos_ReadBuff         ; 9
-	jmp dos_ReadBlock        ; 10
-	jmp dos_GetDirHead       ; 11
-	jmp dos_CalcBlksFree     ; 12
-	jmp dos_Get1stDirEntry   ; 13
-	jmp dos_GetNxtDirEntry   ; 14
+.repeat 7
+	nop
+	sec
+	rts
+.endrepeat
 
 ; init/meta
 	jmp dos_init              ; 15
