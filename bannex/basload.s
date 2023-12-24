1cf28361ce008c5cc9e638d3fdc5a7a651ef553e
mooinglemur <mooinglemur@users.noreply.github.com>
2023-12-24 17:37:36 +0000

    Fix mixture of tabs/spaces in basload.s
    Just some whitespace nitpicking

--- bannex/basload.s
+++ bannex/basload.s
@@ -8,9 +8,9 @@
 
 .proc basload
 	; Check input
-    beq error
+	beq error
 	
-    jsr frmevl ; Evaluate input
+	jsr frmevl ; Evaluate input
 	bit valtyp
 	bmi prepare; Input is a string, try to open file with BASLOAD
 
@@ -21,68 +21,68 @@ prepare:
 	jsr frefac ; Get string
 	beq error ; String is empty
 	
-    ; Store len
+	; Store len
 	sta $02 ; R0L = file name length
 	
-    ; Copy file name
-    lda #BANK_KERNAL
-    sta $00
+	; Copy file name
+	lda #BANK_KERNAL
+	sta $00
 
-    ldy #0
-:   lda ($a9),y
-    sta $bf00,y
-    cpy $02
-    beq :+
-    iny
-    bra :-
+	ldy #0
+:	lda ($a9),y
+	sta $bf00,y
+	cpy $02
+	beq :+
+	iny
+	bra :-
 
-    ; Set current device, if basic_fa < 8 then use default device 8
-:   lda basic_fa
-    cmp #8
-    bcs :+
-    lda #8
-:   sta $03
+	; Set current device, if basic_fa < 8 then use default device 8
+:	lda basic_fa
+	cmp #8
+	bcs :+
+	lda #8
+:  	sta $03
 
-    ; Print "LOADING..."
-    ldx #0
-:   lda msg_loading,x
-    beq :+
-    jsr bsout
-    inx
-    bra :-
+	; Print "LOADING..."
+	ldx #0
+:  	lda msg_loading,x
+	beq :+
+	jsr bsout
+	inx
+	bra :-
 
-    ; Launch BASLOAD
-:   jsr bajsrfar
+	; Launch BASLOAD
+:  	jsr bajsrfar
 	.word $c000
 	.byte BANK_BASLOAD
 	
 response:
-    ; Check if response value != 0
-    lda $04
-    beq exit
+	; Check if response value != 0
+	lda $04
+	beq exit
 
-    ; Response value != 0 => Error
-    ldx #0
-:   lda msg_error,x
-    beq :+
-    jsr bsout
-    inx
-    bra :-
+	; Response value != 0 => Error
+	ldx #0
+:	lda msg_error,x
+	beq :+
+	jsr bsout
+	inx
+	bra :-
 
-:   ldx #0
-    stz $00
-:   lda $bf00,x
-    beq exit
-    jsr bsout
-    inx
-    bra :-
+:  	ldx #0
+	stz $00
+:  	lda $bf00,x
+	beq exit
+	jsr bsout
+	inx
+	bra :-
 
 exit:
-    rts
+	rts
 
 msg_loading:
-    .byt "LOADING...",0
+	.byt "LOADING...",0
 
 msg_error:
-    .byt 13, "ERROR: ",0
+	.byt 13, "ERROR: ",0
 .endproc
