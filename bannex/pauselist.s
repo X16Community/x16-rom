9d5daac4d529afec01488973b2f9f8c90810bf01
Koyaa <90494429+zpc0070@users.noreply.github.com>
2023-12-25 13:19:28 -0600

    buffer test

--- bannex/pauselist.s
+++ bannex/pauselist.s
@@ -3,12 +3,11 @@
 .import crambank
 .import lp_dopause
 .import lp_screenpause
-.import buf
+.import buf 	; BASIC input buffer
 
 .export pause
 
 ram_bank = 0
-input_buffer = $0200
 
 PAGEDOWN = $02
 SPACEBAR = $20
@@ -59,9 +58,9 @@ pause:
 	phx
 	stz	ram_bank	; Set RAM bank 0 for variables
 
-	lda	input_buffer	;$200		; Use BASIC input buffer to see if first run
+	lda	buf		;$200		; Use BASIC input buffer to see if first run
 	beq	@notfirst
-	stz	input_buffer	;$200
+	stz	buf		;$200
 	stz	lp_dopause	; Initialize variables
 	stz	lp_screenpause
 @notfirst:
