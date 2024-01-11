d49fff7ccca980fb5e3480cc828155d9da22dec4
Jimmy Dansbo <jimmy@dansbo.dk>
2024-01-11 11:10:22 +0100

    Ensore OLD function exits if index rolls over. This is to solve the issue where OLD would hang if there is no program present in memory. If index rollover is detected, 0's will be written to the two first bytes of txttab ($0801) to indicate that there is no BASIC program in memory.

--- basic/code1.s
+++ basic/code1.s
@@ -200,7 +200,13 @@ chead	ldy #1
 	beq lnkrts
 	ldy #4
 czloop	iny
-	lda (index),y
+	bne :+		; Continue as normal as long as Y has not rolled over to 0
+	tya		; If Y has rolled over to 0
+	sta (index),y	; store 0's in the two first bytes of the txttab buffer
+	iny		; to show that there is no BASIC program
+	sta (index),y
+	bra lnkrts	; exit function (better with just an RTS?)
+:	lda (index),y
 	bne czloop
 	iny
 	tya
