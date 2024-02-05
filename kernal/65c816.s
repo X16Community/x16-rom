0680e743d7012c0d247870b8417b8414608ced28
George Tokmaji <tokmajigeorge@gmail.com>
2024-02-05 02:40:22 +0100

    Handle DP and SP in native IRQ

--- kernal/65c816.s
+++ kernal/65c816.s
@@ -1,6 +1,6 @@
 .import igetin
 .import cbinv, cinv
-.import __irq, __irq_ret
+.import __irq, __irq_native_ret
 
 .import goto_user, reg_a, reg_x, reg_y
 
@@ -62,15 +62,29 @@ c816_nmib:
 
 c816_irqb:
 :   rep #$30       ; 16-bit accumulator and index
+    .A16
+    .I16
     pha
+
+    phd            ; save DP
+    lda #0000
+    tcd            ; set DP to $0000
+
     sep #$20       ; 8-bit accumulator
+    .A8
     lda rom_bank   ; save ROM bank
     pha
     stz rom_bank   ; set KERNAL bank
     rep #$20       ; 16-bit accumulator
+    .A16
 
     phx            ; save X and Y
     phy
+
+    tsc
+    ldx #$01D0
+    txs            ; set stack pointer to $01D0
+    pha            ; store old stack pointer on new stack
 
     pea c816_irqb_ret ; set up CBM IRQ stack frame
     sec
@@ -79,23 +93,28 @@ c816_irqb:
 
     php
 
+    lda $0B, S
+    pha
     lda $09, S
     pha
     lda $07, S
     pha
-    lda $05, S
-    pha
 
+    .A8
+    .I8
     jmp (cinv)
 
 c816_irqb_ret:
     clc
     xce            ; exit emulation mode
     rep #$31       ; 16-bit accumulator, clear carry
+    pla            ; pull old stack pointer
+    tcs            ; restore stack pointer
     ply
     plx
     sep #$20       ; 8-bit accumulator
-:   jmp __irq_ret
+    pla
+:   jmp __irq_native_ret
 .popcpu
 
 .segment "C816_UTIL"
