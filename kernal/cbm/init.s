db63c3ece7130881a7e3999d404d6396830be999
stefan-b-jakobsson <stefan.b.jakobsson@gmail.com>
2024-02-22 17:42:37 +0200

    Support current SMC and new SMC firmware

--- kernal/cbm/init.s
+++ kernal/cbm/init.s
@@ -6,7 +6,7 @@
 
 .feature labels_without_colons
 
-.import cint, ramtas, ioinit, enter_basic, restor, vera_wait_ready, call_audio_init, boot_cartridge, i2c_restore
+.import cint, ramtas, ioinit, enter_basic, restor, vera_wait_ready, call_audio_init, boot_cartridge, i2c_restore, ps2data_init
 
 .export start
 
@@ -21,6 +21,7 @@ start	ldx #$ff
 	jsr ramtas           ;go ram test and set
 	jsr restor           ;go set up os vectors
 	jsr i2c_restore      ;release I2C pins and clear mutex flag
+	jsr ps2data_init
 ;
 	jsr cint             ;go initilize screen
 	jsr call_audio_init  ;initialize audio API and HW.
--- kernal/drivers/x16/ps2data.s
+++ kernal/drivers/x16/ps2data.s
@@ -5,14 +5,63 @@
 .segment "PS2KBD"
 
 I2C_ADDR = $42
+CMD_GET_KEYCODE = $07
+CMD_GET_MOUSE_MOV = $21
+CMD_GET_VER1 = $30
+CMD_GET_VER2 = $31
+CMD_GET_VER3 = $32
 CMD_SET_DFLT_READ_OP = $40
 CMD_GET_KEYCODE_FAST = $41
 CMD_GET_PS2DATA_FAST = $43
 
-.import i2c_direct_read, i2c_read_next_byte, i2c_read_stop, i2c_write_byte
-.export ps2data_fetch, ps2data_kbd, ps2data_kbd_count, ps2data_mouse, ps2data_mouse_count
+PS2DATA_OLD_STYLE = $01
+PS2DATA_NEW_STYLE = $02
+
+.import i2c_read_byte, i2c_read_first_byte, i2c_direct_read, i2c_read_next_byte, i2c_read_stop, i2c_write_byte
+.export ps2data_init, ps2data_fetch, ps2data_kbd, ps2data_kbd_count, ps2data_mouse, ps2data_mouse_count
 .export ps2data_keyboard_and_mouse, ps2data_keyboard_only
 
+;---------------------------------------------------------------
+; Inits ps2data functions.
+;
+; (1) First determine if the SMC firmware version supports the
+;     new faster method to fetch keyboard and mouse data,
+;     requires version >= 46.0.0
+;
+; (2) Second set default SMC read operation to
+;     fetch keycodes only, if SMC firmare version >= 46.0.0
+; 
+; Input:
+;         Nothing
+; Output:
+;         Data fecth method stored in ps2data_style
+;         - SMC version <= 46.0.0 => PS2DATA_OLD_STYLE
+;         - SMC version > 46.0.0 => PS2DATA_NEW_STYLE
+;---------------------------------------------------------------
+ps2data_init:
+	KVARS_START
+
+	; Compare SMC firmare version major
+	ldx #I2C_ADDR
+	ldy #CMD_GET_VER1
+	jsr i2c_read_byte
+	cmp #$ff
+	beq oldstyle
+	cmp #46
+	bcc oldstyle
+
+newstyle:
+	lda #PS2DATA_NEW_STYLE
+	bra :+
+	
+oldstyle:
+	lda #PS2DATA_OLD_STYLE
+
+:	sta ps2data_style
+	
+	KVARS_END
+
+	jmp ps2data_keyboard_only
 
 ;---------------------------------------------------------------
 ; Set SMC default read operation to fetch key codes only
@@ -24,15 +73,18 @@ CMD_GET_PS2DATA_FAST = $43
 ;---------------------------------------------------------------
 ps2data_keyboard_only:
 	; Clear mouse size
-	lda ram_bank
-	pha
-	stz ram_bank
+	KVARS_START
 	stz ps2data_mouse_size
-	pla
-	sta ram_bank
+	ldx ps2data_style
+	KVARS_END
+
+	; Exit if old style
+	cpx #PS2DATA_OLD_STYLE
+	bne :+
+	rts
 
 	; Send command
-	ldx #I2C_ADDR
+:	ldx #I2C_ADDR
 	ldy #CMD_SET_DFLT_READ_OP
 	lda #CMD_GET_KEYCODE_FAST
 	jmp i2c_write_byte
@@ -48,15 +100,18 @@ ps2data_keyboard_only:
 ;---------------------------------------------------------------
 ps2data_keyboard_and_mouse:
 	; Set mouse size
-	ldx ram_bank
-	phx
-	stz ram_bank
+	KVARS_START
 	sta ps2data_mouse_size
-	plx
-	stx ram_bank
+	ldy ps2data_style
+	KVARS_END
+
+	; Exit if old style
+	cpy #PS2DATA_OLD_STYLE
+	bne :+
+	rts
 	
 	; Send command
-	ldx #I2C_ADDR
+:	ldx #I2C_ADDR
 	ldy #CMD_SET_DFLT_READ_OP
 	lda #CMD_GET_PS2DATA_FAST
 	jmp i2c_write_byte
@@ -85,26 +140,57 @@ ps2data_fetch:
 	stz ps2data_mouse+2
 	stz ps2data_mouse+3
 
-	; Fetch data from SMC
+	; Check if old style
+	lda ps2data_style
+	cmp #PS2DATA_OLD_STYLE
+	bne @1
+
+	; Fetch data from SMC old style
 	ldx #I2C_ADDR
+	ldy #CMD_GET_KEYCODE
+	jsr i2c_read_byte
+	bcs @3
+	cmp #0
+	beq @3
+	bra @2
+
+	; Fetch data from SMC new style
+@1:	ldx #I2C_ADDR
 	jsr i2c_direct_read
 	bcs exit
 	
 	; Store keycode
-	sta ps2data_kbd
+@2:	sta ps2data_kbd
 	inc ps2data_kbd_count
 
 	; Check if to read mouse packet
-	lda ps2data_mouse_size
+@3:	lda ps2data_mouse_size
 	beq done
 
-	; Mouse packet byte 0
-	jsr i2c_read_next_byte
+	; Check if old style
+	lda ps2data_style
+	cmp #PS2DATA_OLD_STYLE
+	bne @4
+
+	; Read mouse packet old style
+	ldx #I2C_ADDR
+	ldy #CMD_GET_MOUSE_MOV
+	jsr i2c_read_first_byte
+	bcs done ; Abort on error
 	sta ps2data_mouse
-	beq done ; Abort if first byte is a zero
+	cmp #0
+	beq done ; Abort if first byte is zero
+	bra @5
+
+	; Mouse packet byte 0
+@4:	jsr i2c_read_next_byte
+	bcs done
+	sta ps2data_mouse
+	cmp #0
+	beq done ; Abort if first byte is zero
 	
 	; Mouse packet byte 1
-	jsr i2c_read_next_byte
+@5:	jsr i2c_read_next_byte
 	sta ps2data_mouse+1
 	
 	; Mouse packet byte 2
@@ -134,3 +220,4 @@ exit:
 	ps2data_mouse: .res 4
 	ps2data_mouse_count: .res 1
 	ps2data_mouse_size: .res 1
+	ps2data_style: .res 1
