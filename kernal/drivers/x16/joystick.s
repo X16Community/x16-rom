0585ade46a72fd82a39828da656a2c1df6df2642
mooinglemur <mooinglemur@users.noreply.github.com>
2024-03-07 00:24:04 -0700

    put delay elsewhere

--- kernal/drivers/x16/joystick.s
+++ kernal/drivers/x16/joystick.s
@@ -70,24 +70,24 @@ joystick_scan:
 	pha
 	pla
 	stz nes_data
-	pha
-	pla
-	pha
-	pla
 
 	; read 3x 8 bits
 	ldx #bit_jclk
 	ldy #8
-l1:	lda nes_data ; Read all controller bits
+l1:	stz nes_data ; Drive NES clock low (NES controller doesn't change when low)
+	pha          ; Delay for slow SNES controllers
+	pla
+	pha
+	pla
+	lda nes_data ; Read all controller bits
 	stx nes_data ; Drive NES clock high
 
-	           ; process while NES clock is high (bits change)
+				; process while NES clock is high (bits change)
 	rol        ; Move bit 7 into C
 	rol joy1   ; Roll C into joy1
 	rol        ; Move bit 6 into C
 	rol joy2   ; Roll C into joy2
 	rol        ; Roll bit 5 into C
-	stz nes_data ; Drive NES clock low (NES controller doesn't change when low)
 	rol joy3   ; Roll C into joy3
 	rol        ; Roll bit 4 into C
 	rol joy4   ; Roll C into joy4
@@ -96,16 +96,20 @@ l1:	lda nes_data ; Read all controller bits
 	bne l1
 
 	ldy #8
-l2:	lda nes_data ; Read all controller bits
+l2:	stz nes_data ; Drive NES clock low (NES controller doesn't change when low)
+	pha          ; Delay for slow SNES controllers
+	pla
+	pha
+	pla
+	lda nes_data ; Read all controller bits
 	stx nes_data ; Drive NES clock high
 
-	           ; process while NES clock is high (bits change)
+				; process while NES clock is high (bits change)
 	rol        ; Move bit 7 into C
 	rol joy1+1 ; Roll C into joy1
 	rol        ; Move bit 6 into C
 	rol joy2+1 ; Roll C into joy2
 	rol        ; Roll bit 5 into C
-	stz nes_data ; Drive NES clock low (NES controller doesn't change when low)
 	rol joy3+1 ; Roll C into joy3
 	rol        ; Roll bit 4 into C
 	rol joy4+1 ; Roll C into joy4
@@ -114,16 +118,20 @@ l2:	lda nes_data ; Read all controller bits
 	bne l2
 
 	ldy #8
-l3:	lda nes_data ; Read all controller bits
+l3:	stz nes_data ; Drive NES clock low (NES controller doesn't change when low)
+	pha          ; Delay for slow SNES controllers
+	pla
+	pha
+	pla
+	lda nes_data ; Read all controller bits
 	stx nes_data ; Drive NES clock high
 
-	           ; process while NES clock is high (bits change)
+				; process while NES clock is high (bits change)
 	rol        ; Move bit 7 into C
 	rol joy1+2 ; Roll C into joy1
 	rol        ; Move bit 6 into C
 	rol joy2+2 ; Roll C into joy2
 	rol        ; Roll bit 5 into C
-	stz nes_data ; Drive NES clock low (NES controller doesn't change when low)
 	rol joy3+2 ; Roll C into joy3
 	rol        ; Roll bit 4 into C
 	rol joy4+2 ; Roll C into joy4
