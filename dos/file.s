a2b92e56ea1b192cebf24689db0d6f27d2f94923
mooinglemur <mooinglemur@users.noreply.github.com>
2025-04-11 20:40:16 -0700

    fix comment

--- dos/file.s
+++ dos/file.s
@@ -322,7 +322,7 @@ file_read_block_long:
 ;       c         =0: regular save from memory
 ;                 =1: stream from single address (e.g. VERA_data0)
 ;                     (only has an effect if the bank byte (r1L) 0)
-; Out:  r1        number of bytes written
+; Out:  r2        number of bytes written
 ;       c         =1: error
 ;---------------------------------------------------------------
 file_write_block_long:
